c2fbc0a2fe9e2b683565004adbc5dc4404aca223
Sam Scherl <sscherl2017@pingry.org>
2016-01-31 22:11:25 -0500

    commented and everything still working

--- HuffmanNode.java
+++ HuffmanNode.java
@@ -1,14 +1,23 @@
 public class HuffmanNode implements Comparable<HuffmanNode>
 {
+	/* The key of the HuffmanNode. */
 	private String key;
 	
+	/* The value of the HuffmanNode. */
 	private int value;
 	
+	/** A pointer to the HuffmanNode to the left of the HuffmanNode. */
 	protected HuffmanNode left;
 	
-	/** A pointer to the HuffmanNode to the right of this HuffmanNode. */
+	/** A pointer to the HuffmanNode to the right of the HuffmanNode. */
 	protected HuffmanNode right;
 	
+	/**
+	* A constructor for HuffmanNodes that takes in a String and an int and leaves the left and
+	* right HuffmanNodes as null.
+	* @param k	The key for the HuffmanNode
+	* @param v	The value for the HuffmanNode
+	*/
 	public HuffmanNode(String k, int v)
 	{
 		key = k;
@@ -20,6 +29,7 @@ public class HuffmanNode implements Comparable<HuffmanNode>
 	/**
 	* A Constructor which initializes the HuffmanNode with the given value and the given HuffmanNodes to the left
 	* and right of it. 
+	* @param k	The key of the HuffmanNode
 	* @param v	The value of the HuffmanNode
 	* @param l	The pointer to the HuffmanNode to the left
 	* @param r	The pointer to the HuffmanNode to the right
@@ -28,8 +38,8 @@ public class HuffmanNode implements Comparable<HuffmanNode>
 	{
 		key = k;
 		value = v;
-		setLeft(l);
-		setRight(r);
+		left = l;
+		right = r;
 	}
 	
 	/**
@@ -50,6 +60,10 @@ public class HuffmanNode implements Comparable<HuffmanNode>
 		return right;
 	}
 	
+	/*
+	* Returns the key for the HuffmanNode.
+	* @return	The key for the HuffmanNode
+	*/
 	public String getKey()
 	{
 		return key;
@@ -65,24 +79,6 @@ public class HuffmanNode implements Comparable<HuffmanNode>
 	}
 	
 	/**
-	* Sets HuffmanNode to the left to the given HuffmanNode.
-	* @param node	The HuffmanNode which will be set as the left HuffmanNode
-	*/
-	public void setLeft(HuffmanNode node)
-	{
-		left = node;
-	}
-	
-	/**
-	* Sets HuffmanNode to the right to the given HuffmanNode.
-	* @param node	The HuffmanNode which will be set as the right HuffmanNode
-	*/	
-	public void setRight(HuffmanNode node)
-	{
-		right = node;
-	}
-	
-	/**
 	* Returns true if the HuffmanNode is a leaf, false if it is not.
 	* @return	True if the HuffmanNode is a leaf, false if it is not
 	*/
@@ -91,6 +87,10 @@ public class HuffmanNode implements Comparable<HuffmanNode>
 		return (left == null && right == null);
 	}
 	
+	/**
+	* Returns a String representation of the HuffmanNode.
+	* @return	A String representation of the HuffmanNode
+	*/
 	public String toString()
 	{
 		if (isLeaf())
@@ -105,6 +105,11 @@ public class HuffmanNode implements Comparable<HuffmanNode>
 		}
 	}
 	
+	/**
+	* Compares the value of the HuffmanNode to the value of another HuffmanNode.
+	* @param other	The HuffmanNode that will be compared against
+	* @return	The difference between the values of the two HuffmanNodes
+	*/
 	public int compareTo(HuffmanNode other)
 	{
 		return value - other.value;
--- HuffmanRunner.java
+++ HuffmanRunner.java
@@ -2,19 +2,40 @@ import java.util.HashMap;
 
 import java.util.PriorityQueue;
 
+/**
+* This class contains functions that can turn a String into a HashMap, turn a HashMap into a PriorityQueue of HuffmanNodes,
+* turn a PriorityQueue into a tree of HuffmanNodes. Using the input and the tree, this class can create a String of bits
+* of the minimum length to encode the input. Using the the tree and String of bits it can recreate the original input. I
+* use a HashMap because you can access and add to it in constant time.
+* @author Sam Scherl
+* @version 12/14/15
+*/
 public class HuffmanRunner
 {
+	/* The String that will be encoded. */
 	private static String input;
 	
+	/* The HuffmanNode which contains all the other HuffmanNodes. */
 	private static HuffmanNode tree;
 	
+	/**
+	* The main class which takes a String and calls all the functions necessary to convert it into Huffman code.
+	* @param args	This is not used
+	*/
 	public static void main(String[] args)
 	{
 		input = "mississippi river";
 		tree = createTree(createPriorityQueue(createMap()));
+		System.out.println(bitCreator());
+		System.out.println(bitCreator().length());
 		System.out.println(bitReader(bitCreator()));
 	}
 	
+	/**
+	* This traverses through the input and creates a HashMap that contains all the characters 
+	* with the amount of times they occur.
+	* @return	The HashMap that contains all the characters with the amount of times they occur.
+	*/
 	public static HashMap<String, Integer> createMap()
 	{
 		HashMap<String, Integer> map = new HashMap<String, Integer>(input.length() * 2);
@@ -28,6 +49,12 @@ public class HuffmanRunner
 		return map;
 	}
 	
+	/**
+	* This traverses a HashMap and populates a PriorityQueue filled with HuffmanNodes which represent individual
+	* characters and their respective occurrences.
+	* @param map	The HashMap that will be traversed
+	* @return	A PriorityQueue of HuffmanNodes
+	*/
 	public static PriorityQueue<HuffmanNode> createPriorityQueue(HashMap<String, Integer> map)
 	{
 		PriorityQueue<HuffmanNode> queue = new PriorityQueue<HuffmanNode>();
@@ -36,6 +63,11 @@ public class HuffmanRunner
 		return queue;
 	}
 	
+	/**
+	* Traverses through a PriorityQueue and creates a HuffmanNode that contains all the other HuffmanNodes.
+	* @param queue	The PriorityQueue that will be traversed
+	* @return	A HuffmanNode that contains all the other HuffmanNodes from the PriorityQueue
+	*/
 	public static HuffmanNode createTree(PriorityQueue<HuffmanNode> queue)
 	{
 		HuffmanNode combined = new HuffmanNode("", 0);
@@ -53,9 +85,18 @@ public class HuffmanRunner
 		return combined;
 	}
 	
+	/**
+	* Turns the given input into a String of bits, of the least possible length, based on the Huffman tree that was created.
+	* @return	A String of bits that represents the input
+	*/
 	public static String bitCreator()
 	{
 		String output = "";
+		if (tree.isLeaf())
+		{
+			for (int j = 0; j < input.length(); j++)
+				output += "0";
+		}
 		for (int i = 0; i < input.length(); i++)
 		{
 			HuffmanNode current = tree;
@@ -76,10 +117,21 @@ public class HuffmanRunner
 		return output;
 	}
 	
+	/**
+	* Turns a String of bits into a String of text based on the Huffman tree that was created.
+	* @param bits	A String of bits that represents text
+	* @return	The original input that was converted into bits.
+	*/
 	public static String bitReader(String bits)
 	{
 		String output = "";
 		int i = 0;
+		if (tree.isLeaf())
+		{
+			for (int j = 0; j < bits.length(); j++)
+				output += tree.getKey();
+			return output;
+		}
 		while (i < bits.length())
 		{
 			HuffmanNode current = tree;
